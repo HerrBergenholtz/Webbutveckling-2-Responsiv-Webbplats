65d417ae8c207451c34272d9018aeb70f5c6fa5b
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-14 09:14:58 +0100

    Onsdag lektion

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ let section;
 let jsonArr = [
     article1 = {
         img: "../src/våt uggl.jpg",
-        caption: "bildtext",
-        text: "dfjkasdfkjadskjfasdklfsndajaksvdhixczjnkasjdfn"
+        caption: "bildtsdasdsad ext",
+        txt: "dfjkasdfk jadskjfasdkl fsndajaksvdhix czjnkasjdfn"
     },
 
     article1 = {
         img: "../src/uggl.PNG",
         caption: "bildtext",
-        text: "dfjkasdfkjadskjfasdklfsndajaksvdhixczjnkasjdfn"
+        txt: "dfjkas dfkjads kjfasdklfs dajaksvdh ixczjnk asjdfn"
     },
 
     article1 = {
         img: "../src/våt uggl.jpg",
         caption: "bildtext",
-        text: "dfjkasdfkjadskjfasdklfsndajaksvdhixczjnkasjdfn"
+        txt: "dfjk asdfkjadskjfasdk lfsndajak svdh ixczjn kasj dfn"
     },
 
     article1 = {
         img: "../src/våt uggl.jpg",
         caption: "bildtext",
-        text: "dfjkasdfkjadskjfasdklfsndajaksvdhixczjnkasjdfn"
+        txt: "dfjk asdfkjadsk jfasdklfs ndajaksvdhi xczj nkasjdfn"
     }
 ];
 
@@ -31,7 +31,23 @@ window.onload = () => {
 }
 
 function addArticles() {
-    for (let i = 0; i <= 4; i++) {
-        section.innerHTML += "<article><figure><img src=\"" + jsonArr[i].img + "\"><figcaption>" + jsonArr[i].caption + "</figcaption></figure><p>" + jsonArr[i].text + "</p></article>"
+    for (let i = 0; i < 4; i++) {
+        const article = document.createElement("article");
+        const fig = document.createElement("figure");
+        const img = document.createElement("img");
+        const caption = document.createElement("figcaption");
+        const text = document.createElement("p");
+
+        text.innerText = jsonArr[i].txt;
+        caption.innerText = jsonArr[i].caption;
+        img.src = jsonArr[i].img;
+        img.alt = "uggla";
+        fig.appendChild(img);
+        fig.appendChild(caption);
+        article.appendChild(fig)
+        article.appendChild(text);
+        section.appendChild(article);
+
+        //section.innerHTML += "<article><figure><img src=\"" + jsonArr[i].img + "\"><figcaption>" + jsonArr[i].caption + "</figcaption></figure><p>" + jsonArr[i].text + "</p></article>"
     }
 }
